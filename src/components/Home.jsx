660acc83f95f6759d45f8adff27732da6d8f8f7f
soniarez <101676781+soniarez@users.noreply.github.com>
2022-08-13 14:15:09 -0400

    home view

---
+++ src/components/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import soniaImg from '../assets/soniaImg.JPG';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className='flex flex-col justify-center h-full'>
+          <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a FrontEnd Developer</h2>
+          <p className='text-gray-500 py-4 max-w-md text-justify'>
+            I'm Sonia Reyes, passionate about tecnology and creating things, getting to know
+            and understand how things work. I also have a background and
+            experience in Business Administration.
+            <br /> 
+            Currently, I'm working mostly with React, JavaScript, HTML5 and CSS3.
+          </p>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-teal-500 to-emerald-500">
+              Portfolio
+              <span className='group-hover:rotate-90 duration-300'>
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <img src={soniaImg} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-full' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+const NavBar = () => {
+  const [hamburgerMenu, setHamburgerMenu] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: 'home',
+    },
+    {
+      id: 2,
+      link: 'about',
+    },
+    {
+      id: 3,
+      link: 'portfolio',
+    },
+    {
+      id: 4,
+      link: 'skills',
+    },
+    {
+      id: 5,
+      link: 'contact',
+    },
+  ];
+
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Sonia</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(link => (
+          <li
+            key={link.id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            {link.link}
+          </li>
+        ))}
+      </ul>
+      <div
+        onClick={() => setHamburgerMenu(!hamburgerMenu)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {hamburgerMenu ? <FaTimes size={30} /> : <FaBars sise={30} />}
+      </div>
+      {hamburgerMenu && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-500">
+          {links.map(link => (
+            <li
+              key={link.id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              {link.link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
