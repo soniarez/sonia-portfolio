218e8c35e2e7b746510edeaae6fb9b48254560fc
soniarez <101676781+soniarez@users.noreply.github.com>
2022-08-14 00:31:59 -0400

    my about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,20 +11,27 @@ const About = () => {
           <p className="text-4xl font-bold inline border-b-4 border-teal-500">
             About:
           </p>
-          <p className='text-xl mt-20'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+          <p className="text-xl mt-20">
+            Welcome to my site!
+            <br />
+            <br />
+            My name is Sonia Reyes, and as you may have noticed, I'm a Front End
+            Developer based in Chile. But I haven't always been a web developer.
+            I studied, and for years worked in the business related field.
           </p>
-
           <br />
-
+          <p className="text-xl">
+            In 2019 I had the amazing opportunity of getting into Start-Up Chile
+            with a project focused on construtech. And from there, I fell in
+            love with the tech world. I took a leap of faith and made my
+            decition that coding was my thing. That is how in 2022 I applied and
+            was selected to enter Laboratoria's full-time FrontEnd Bootcamp.
+          </p>
+          <br />
           <p className='text-xl'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            Right now, I work mostly with React, Javascript, HTML and CSS. Also,
+            my background in business has helped me to create projects with a
+            broad vision that seeks to create and improve user experience.
           </p>
         </div>
       </div>
